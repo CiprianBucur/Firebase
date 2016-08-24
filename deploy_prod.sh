6ac50bcac0962d4499c97905e5a8965a3d51ef96
CiprianBucur <ciprian-git@outlook.com>
2016-08-24 15:34:41 +0300

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-ssh -v root@YOUR_IP_HERE << EOF
-
-echo '1. Updating sources'
-cd Firebase
-git checkout --force master
-git pull
-
-echo "2. Restart apache"
-sudo apache2ctl graceful
-
 echo 'Done!'
 
-EOF
